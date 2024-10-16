6df6e5ef722e85b621d71f2734eab97a17d3dc91
pietonov <pietonov@msu.edu>
2024-10-16 16:27:12 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 
 # Load the summarized data
 def load_data():
@@ -117,6 +118,7 @@ bin_edges_extended = pd.concat([aggregated_bins['bin_edges'], pd.Series([aggrega
 # Plot the dynamically aggregated histogram
 plt.figure(figsize=(10, 6))
 plt.bar(aggregated_bins['bin_edges'], aggregated_bins['frequency'], width=np.diff(bin_edges_extended), color='blue', alpha=0.7, edgecolor='black')
+sns.kdeplot(df['LOG_GROUND_TIME'], color='darkblue', linewidth=2)
 plt.title('Log-Transformed Distribution of GROUND_TIME')
 plt.xlabel('Log(GROUND_TIME)')
 plt.ylabel('Frequency')
