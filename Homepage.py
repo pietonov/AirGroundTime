864716d41376329bc508b366528412e85e36c95f
Victor Pietono <pietonov@msu.com>
2024-12-04 21:26:41 -0500

    aaa

--- Homepage.py
+++ Homepage.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="Ground Time Predictor", layout="wide")
 
 
 st.title("Ground Time Predictor 🛬")
-st.image("DATA/GroundTimePredictor.png", caption="Ground Operations Optimization", use_column_width=False)
+st.image("DATA/GroundTimePredictor.png", caption="Ground Operations Optimization", width=800)
 
 st.write("""
 Welcome to the **Ground Time Predictor**, a tool designed to estimate the duration of ground operations for flights based on input parameters such as airport, aircraft type, and operational conditions.
